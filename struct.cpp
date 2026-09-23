3f60b0e9e773284422fe4ede7815e949a27bb3f8
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-23 23:59:25 +0530

    class example

---
+++ struct.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class name1 {
+    public:
+    string name = "Rohan";
+    int grade = 99;
+    int rollno = 1;
+
+};
+
+class name2 {
+    public:
+    string name = "Chirag";
+    int grade = 100;
+    int rollno = 2;
+
+};
+
+class name3 {
+    public:
+    string name = "Ronaldo";
+    int grade = 88;
+    int rollno = 3;
+
+};
+
+int main() {
+    name1 s1;
+    name2 s2;
+    name3 s3;
+
+
+
+   
